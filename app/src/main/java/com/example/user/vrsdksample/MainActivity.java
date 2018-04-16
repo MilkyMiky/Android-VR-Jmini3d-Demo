7e635b1720e3c5f9206fb50299ffc69b16a12f80
Mikhail Lysyansky <mikhail.lysyansky@jetruby.com>
2018-04-16 17:43:26 +0300

    Set GVR view

---
+++ app/src/main/java/com/example/user/vrsdksample/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.user.vrsdksample;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.vr.sdk.base.AndroidCompat;
+import com.google.vr.sdk.base.Eye;
+import com.google.vr.sdk.base.GvrActivity;
+import com.google.vr.sdk.base.GvrView;
+import com.google.vr.sdk.base.HeadTransform;
+import com.google.vr.sdk.base.Viewport;
+
+import javax.microedition.khronos.egl.EGLConfig;
+
+public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        GvrView gvrView = findViewById(R.id.gvr_view);
+        gvrView.setRenderer(this);
+        setGvrView(gvrView);
+    }
+
+    @Override
+    public void onNewFrame(HeadTransform headTransform) {
+
+    }
+
+    @Override
+    public void onDrawEye(Eye eye) {
+
+    }
+
+    @Override
+    public void onFinishFrame(Viewport viewport) {
+
+    }
+
+    @Override
+    public void onSurfaceChanged(int width, int height) {
+
+    }
+
+    @Override
+    public void onSurfaceCreated(EGLConfig config) {
+
+    }
+
+    @Override
+    public void onRendererShutdown() {
+
+    }
+}
